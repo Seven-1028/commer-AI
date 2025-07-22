40032639e584933ff2ce274708974f2737eaae49
滑滑梯 <3423891463@qq.com>
2025-07-23 02:18:19 +0800

    element-plus按需引入

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
+//按需导入element-plus
+import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
@@ -9,8 +14,16 @@ export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    //element-plus配置插件
+    AutoImport({
+      resolvers: [ElementPlusResolver()],
+    }),
+    Components({
+      resolvers: [ElementPlusResolver()],
+    }),
   ],
   resolve: {
+    //实际的路径转换 @->src
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
